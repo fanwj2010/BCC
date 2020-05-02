acffba56c3d28192ba7d706a59018f46bf766f0c
Wenjun Fan <fanwenjun2010@gmail.com>
2020-05-02 13:56:11 -0600

    Create bcc_t_sender.py

---
+++ bcc_t_sender.py
@@ -0,0 +1,46 @@
+import time
+import zmq
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import ast
+
+random_generator = Random.new().read
+key = RSA.generate(1024, random_generator)
+publickey = key.publickey()
+#privatekey = key.privatekey()
+
+msg_plaintext_str = "alert tcp 10.0.2.5 any -> any 80 (msg:\"DROP\"; sid:1000008; priority:0;)"
+#msg_plaintext_str = "10.0.2.5"
+msg_encrypt = publickey.encrypt(msg_plaintext_str, 32)
+msg_encrypt_str = str(msg_encrypt)
+#print(msg_encrypt_str)
+
+context = zmq.Context()
+
+print("Connection to the Controller3")
+socket3 = context.socket(zmq.REQ)
+socket3.connect("tcp://10.0.2.4:5555")
+
+print("Connection to the Controller1")
+socket1 = context.socket(zmq.REQ)
+socket1.connect("tcp://10.0.2.7:5555")
+
+for request in range(10):
+    #print("Sending request %s..." % request)
+    t_start = time.time()
+#   sleep.time(1)
+    #socket.send(u"alert tcp 10.0.2.5 any -> any 80 (msg:\"DROP\"; sid:1000008; priority:0;)")
+    #socket.send(msg_plaintext_str)
+    #socket3.send(msg_encrypt_str)
+    socket1.send(msg_encrypt_str)
+    #message3 = socket3.recv()
+    message1 = socket1.recv()
+    t_end = time.time()
+    t_dur = t_end - t_start
+    #time.sleep(0.1)
+    #print("Received reply %s [%s]" %(request,message3))
+    print("Received reply %s [%s]" %(request,message1))
+   # print("latency %s" %(t_dur))
+    print( (t_dur))
+
